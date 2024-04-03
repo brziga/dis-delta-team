0c2263e007ff5c31d11d81023196de344bb3e978
Lukas Kneissl <KneisslLukas@web.de>
2024-04-03 14:45:00 +0200

    removed dis-tutorial3 and added experiments

---
+++ experiments/experiment 2/detect_people.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data, QoSReliabilityPolicy
+
+from sensor_msgs.msg import Image, PointCloud2
+from sensor_msgs_py import point_cloud2 as pc2
+
+from visualization_msgs.msg import Marker
+
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import numpy as np
+
+from ultralytics import YOLO
+
+from delta_interfaces.msg import DetectedObjects
+
+# from rclpy.parameter import Parameter
+# from rcl_interfaces.msg import SetParametersResult
+
+class detect_faces(Node):
+
+	def __init__(self):
+		super().__init__('detect_faces')
+
+		self.declare_parameters(
+			namespace='',
+			parameters=[
+				('device', ''),
+		])
+
+		marker_topic = "/people_marker"
+
+		self.detection_color = (0,0,255)
+		self.device = self.get_parameter('device').get_parameter_value().string_value
+
+		self.bridge = CvBridge()
+		self.scan = None
+
+		self.rgb_image_sub = self.create_subscription(Image, "/oakd/rgb/preview/image_raw", self.rgb_callback, qos_profile_sensor_data)
+		self.pointcloud_sub = self.create_subscription(PointCloud2, "/oakd/rgb/preview/depth/points", self.pointcloud_callback, qos_profile_sensor_data)
+
+		self.marker_pub = self.create_publisher(Marker, marker_topic, QoSReliabilityPolicy.BEST_EFFORT)
+		
+		# publishing detected objects
+		self.object_publisher = self.create_publisher(DetectedObjects, 'some_detected_object', 1)
+
+		self.model = YOLO("yolov8n.pt")
+
+		self.faces = []
+		
+		self.bounding_box = []
+
+		self.get_logger().info(f"Node has been initialized! Will publish face markers to {marker_topic}.")
+
+	def rgb_callback(self, data):
+
+		self.faces = []
+		
+		self.bounding_box = []
+
+		try:
+			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+
+			self.get_logger().info(f"Running inference on image...")
+
+			# run inference
+			res = self.model.predict(cv_image, imgsz=(256, 320), show=False, verbose=False, classes=[0], device=self.device)
+
+			# iterate over results
+			for x in res:
+				bbox = x.boxes.xyxy
+				if bbox.nelement() == 0: # skip if empty
+					continue
+
+				self.get_logger().info(f"Person has been detected!")
+
+				bbox = bbox[0]
+
+				# draw rectangle
+				cv_image = cv2.rectangle(cv_image, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), self.detection_color, 3)
+
+				cx = int((bbox[0]+bbox[2])/2)
+				cy = int((bbox[1]+bbox[3])/2)
+
+				# draw the center of bounding box
+				cv_image = cv2.circle(cv_image, (cx,cy), 5, self.detection_color, -1)
+
+				self.faces.append((cx,cy))
+				
+				# rectangle points (x,y):
+				#
+				# bbox[0],bbox[3]----------------bbox[2],bbox[3]
+				# |                                           |
+				# bbox[0],bbox[1]----------------bbox[2],bbox[1]
+				bb1x = int(bbox[0])
+				bb1y = int(bbox[1])
+				bb2x = int(bbox[0])
+				bb2y = int(bbox[3])
+				bb3x = int(bbox[2])
+				bb3y = int(bbox[3])
+				bb4x = int(bbox[2])
+				bb4y = int(bbox[1])
+				self.bounding_box.append((bb1x, bb1y, bb2x, bb2y, bb3x, bb3y, bb4x, bb4y))
+
+			cv2.imshow("image", cv_image)
+			key = cv2.waitKey(1)
+			if key==27:
+				print("exiting")
+				exit()
+			
+		except CvBridgeError as e:
+			print(e)
+
+	def pointcloud_callback(self, data):
+
+		# get point cloud attributes
+		height = data.height
+		width = data.width
+		point_step = data.point_step
+		row_step = data.row_step		
+
+		# iterate over face coordinates
+		for x,y in self.faces:
+
+			# get 3-channel representation of the poitn cloud in numpy format
+			a = pc2.read_points_numpy(data, field_names= ("x", "y", "z"))
+			a = a.reshape((height,width,3))
+
+			# read center coordinates
+			d = a[y,x,:]
+
+			# create marker
+			marker = Marker()
+
+			marker.header.frame_id = "/base_link"
+			marker.header.stamp = data.header.stamp
+
+			marker.type = 2
+			marker.id = 0
+
+			# Set the scale of the marker
+			scale = 0.1
+			marker.scale.x = scale
+			marker.scale.y = scale
+			marker.scale.z = scale
+
+			# Set the color
+			marker.color.r = 1.0
+			marker.color.g = 1.0
+			marker.color.b = 1.0
+			marker.color.a = 1.0
+
+			# Set the pose of the marker
+			marker.pose.position.x = float(d[0])
+			marker.pose.position.y = float(d[1])
+			marker.pose.position.z = float(d[2])
+
+			self.marker_pub.publish(marker)
+			
+		for bb1x, bb1y, bb2x, bb2y, bb3x, bb3y, bb4x, bb4y in self.bounding_box:
+		        # get 3-channel representation of the poitn cloud in numpy format
+			a = pc2.read_points_numpy(data, field_names= ("x", "y", "z"))
+			a = a.reshape((height,width,3))
+			
+			# read corner coordinates
+			p1 = a[bb1y,bb1x,:]
+			p2 = a[bb2y,bb2x,:]
+			p3 = a[bb3y,bb3x,:]
+			p4 = a[bb4y,bb4x,:]
+			
+			# now creating arrays of the points
+			p1_array = [float(p1[0]), float(p1[1]), float(p1[2])]
+			p2_array = [float(p2[0]), float(p2[1]), float(p2[2])]
+			p3_array = [float(p3[0]), float(p3[1]), float(p3[2])]
+			p4_array = [float(p4[0]), float(p4[1]), float(p4[2])]
+			
+			# creating numpy vectors
+			vector1 = np.array(p1_array)
+			vector2 = np.array(p2_array)
+			vector3 = np.array(p3_array)
+			vector4 = np.array(p4_array)
+			
+			# calculating the vectors we need
+			v1 = vector1 - vector3
+			v2 = vector2 - vector4
+			
+			perpendicular_vector = np.cross(v1, v2)
+			# making it length 1
+			perpendicular_unit_vector = perpendicular_vector / np.linalg.norm(perpendicular_vector)
+			
+			# center of image
+			v_center = 0.5 * (vector1 + vector3)
+			
+			# getting result
+			scale = 0.3
+			v_result1 = v_center + (scale * perpendicular_unit_vector)
+			v_result2 = v_center - (scale * perpendicular_unit_vector)
+			# choosing correct result (the one closer to robot -> the shorter one bc we are in robot frame)
+			v_final_result = v_result1
+			if np.linalg.norm(v_result2) <= np.linalg.norm(v_result1):
+			    v_final_result = v_result2
+			
+			# create marker
+			marker = Marker()
+
+			marker.header.frame_id = "/base_link"
+			marker.header.stamp = data.header.stamp
+
+			marker.type = 2
+			marker.id = 0
+
+			# Set the scale of the marker
+			scale = 0.1
+			marker.scale.x = scale
+			marker.scale.y = scale
+			marker.scale.z = scale
+
+			# Set the color
+			marker.color.r = 1.0
+			marker.color.g = 0.5
+			marker.color.b = 0.0
+			marker.color.a = 1.0
+
+			# Set the pose of the marker
+			marker.pose.position.x = v_final_result[0]
+			marker.pose.position.y = v_final_result[1]
+			marker.pose.position.z = v_final_result[2]
+			
+			#self.marker_pub.publish(marker)
+			
+			# now sending message to object_identifier
+			msg = DetectedObjects()
+			msg.position_x = v_center[0]
+			msg.position_y = v_center[1]
+			msg.position_z = v_center[2]
+			msg.park_position_x = v_final_result[0]
+			msg.park_position_y = v_final_result[1]
+			msg.park_position_z = v_final_result[2]
+			self.object_publisher.publish(msg)
+			
+			
+
+def main():
+	print('Face detection node starting.')
+
+	rclpy.init(args=None)
+	node = detect_faces()
+	rclpy.spin(node)
+	node.destroy_node()
+	rclpy.shutdown()
+
+if __name__ == '__main__':
+	main()
---
+++ experiments/experiment 2/object_identifier.py
@@ -0,0 +1,245 @@
+import rclpy
+from rclpy.node import Node
+
+from delta_interfaces.msg import LevelObjects
+from delta_interfaces.msg import DetectedObjects
+
+# for receiving marker from detect_people node
+from visualization_msgs.msg import Marker
+
+# for transforming between coordinate frames
+from tf2_ros.buffer import Buffer
+from tf2_ros import TransformException
+from tf2_ros.transform_listener import TransformListener
+from rclpy.duration import Duration
+from geometry_msgs.msg import PointStamped
+import tf2_geometry_msgs as tfg
+
+# publishing markers
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import PointStamped
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy
+from builtin_interfaces.msg import Duration
+
+
+# structure to store the currently known level objects
+class CurrentLevelObjects:
+    def __init__(self, position_x, position_y, position_z, rotation, object_id, number_of_objects):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.position_z = position_z
+        self.rotation = rotation
+        self.object_id = object_id
+        self.number_of_objects = number_of_objects
+
+class LevelObjectIdentifier(Node):
+
+    def __init__(self):
+        super().__init__('level_object_identifier')
+        # initialize member variables
+        self.current_level_objects_ = CurrentLevelObjects([], [], [], [], [], 0)
+        self.personId = 1
+        
+        # creating the publisher and a timer to publish level objects regulary
+        self.publisher_ = self.create_publisher(LevelObjects, 'level_objects', 1)
+        timer_period = 1.0  # seconds
+        self.timer = self.create_timer(timer_period, self.publish_level_objects)
+        
+        # subscriber to receive objects from detected_people
+        self.object_subscription = self.create_subscription(DetectedObjects, "/some_detected_object", self.receive_marker, 1)
+        self.object_subscription  # prevent unused variable warning
+        
+        # for transforming between coordinate frames
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+        
+        # For publishing the markers
+        self.marker_pub = self.create_publisher(Marker, "/delta_nav_marker", QoSReliabilityPolicy.RELIABLE)
+        
+        #for testing
+        #self.insert_level_object(1.0, 2.0, .3, 0.0,"person_1")
+        #self.insert_level_object(-1.0, -2.0, .3, 0.0,"person_2")
+        #self.insert_level_object(1.0, 4.0, .3, 0.0,"person_3")
+        
+
+    # gets called every time the subscriber receives a face marker
+    def receive_marker(self, msg):
+        robot_frame_x = msg.position_x
+        robot_frame_y = msg.position_y
+        robot_frame_z = msg.position_z
+        park_x = msg.park_position_x
+        park_y = msg.park_position_y
+        park_z = msg.park_position_z
+        
+        
+        # transforming from robot frame to map frame
+        face_map_position = self.transform_from_robot_to_map_frame(robot_frame_x, robot_frame_y, robot_frame_z)
+        park_map_position = self.transform_from_robot_to_map_frame(park_x, park_y, park_z)
+        if (face_map_position is not None) and (park_map_position is not None):
+            self.process_face_position_in_map_frame(face_map_position[0], face_map_position[1] ,face_map_position[2], park_map_position[0], park_map_position[1], park_map_position[2])
+
+
+    # processes the position of a face that was spottet in the map coordinate frame at face_x, face_y, face_z
+    def process_face_position_in_map_frame(self, face_x, face_y ,face_z, park_x, park_y, park_z):
+    
+        # check if the new face is close to any of the known faces
+        distance_threshold = 0.5
+        distance_threshold_squared = distance_threshold * distance_threshold
+        for i in range(self.current_level_objects_.number_of_objects):
+            x = self.current_level_objects_.position_x[i]
+            y = self.current_level_objects_.position_y[i]
+            z = self.current_level_objects_.position_z[i]
+            
+            dx = x - face_x
+            dy = y - face_y
+            dz = z - face_z
+            
+            dist_squared = dx * dx + dy * dy + dz * dz
+            
+            if (dist_squared < distance_threshold_squared):
+                # this face already exists!
+                self.get_logger().info('ALREADY KNOWN person detected at (map_frame): (x: %f  y: %f  z: %f)' % (face_x, face_y, face_z))
+                return
+        
+        # face is not close to any of the known faces -> it must be a new face!
+        rotation = 0.0
+        self.insert_level_object(face_x, face_y, face_z, rotation,"person_"+str(self.personId))
+        self.personId = self.personId + 1
+        self.get_logger().info('FOUND A NEW person detected at (map_frame): (x: %f  y: %f  z: %f)' % (face_x, face_y, face_z))
+        
+        # TODO: - This node is currenty using the marker messages from the detect_people script of dis_tutorial3. I guess it would be
+        #         better to implement an other msg-topic instead of using the marker-msg. Maybe migrate the detect_people script into our project?
+        #       - Best might be not to set the face position itself as the object position, but a position about 1 meter before the face and
+        #         with a rotation so that the robot faces the person when moving to the x,y,z,rot of the stored object
+
+
+    # trys to transfer the given point from robot frame to map frame.
+    # Returns the array [map_frame_x, map_frame_y, map_frame_z]. If it can not succeede it returns None
+    def transform_from_robot_to_map_frame(self, robot_frame_x, robot_frame_y, robot_frame_z):
+        point_in_robot_frame = PointStamped()
+        point_in_robot_frame.header.frame_id = "/base_link"
+        point_in_robot_frame.header.stamp = self.get_clock().now().to_msg()
+        point_in_robot_frame.point.x = robot_frame_x
+        point_in_robot_frame.point.y = robot_frame_y
+        point_in_robot_frame.point.z = robot_frame_z
+        
+        time_now = rclpy.time.Time()
+        timeout = rclpy.duration.Duration(seconds=0.1)
+
+        try:
+            trans = self.tf_buffer.lookup_transform("map", "base_link", time_now, timeout)
+            point_in_map_frame = tfg.do_transform_point(point_in_robot_frame, trans)
+            map_frame_x = point_in_map_frame.point.x
+            map_frame_y = point_in_map_frame.point.y
+            map_frame_z = point_in_map_frame.point.z
+            return [map_frame_x, map_frame_y, map_frame_z]
+        except TransformException as te:
+            self.get_logger().info(f"Cound not get the transform: {te}")
+            return None
+
+
+    # inserts the given level object into the set of known level objects
+    def insert_level_object(self, object_position_x, object_position_y, object_position_z, object_rotation, object_id):
+        # getting pointer to current level objects
+        old_current_level_objects = self.current_level_objects_
+        # creating a new CurrentLevelObjects object for the member variable 'current_level_objects_'
+        self.current_level_objects_ = CurrentLevelObjects(
+            old_current_level_objects.position_x + [object_position_x],
+            old_current_level_objects.position_y + [object_position_y],
+            old_current_level_objects.position_z + [object_position_z],
+            old_current_level_objects.rotation + [object_rotation],
+            old_current_level_objects.object_id + [object_id],
+            old_current_level_objects.number_of_objects + 1
+        )
+        # publish current level objects whenever something changes about them (plus periodically via timer)
+        self.publish_level_objects()
+
+
+    # is called regulary by a timer to publish all known level objects
+    def publish_level_objects(self):
+        # getting pointer to current_level_objects before publishing, in case the object is exchanged while building the message
+        current_level_objects = self.current_level_objects_
+        msg = LevelObjects() # creating msg of type LevelObjects interface (see import above: from delta_interfaces.msg import LevelObjects)
+        msg.position_x = current_level_objects.position_x
+        msg.position_y = current_level_objects.position_y
+        msg.position_z = current_level_objects.position_z
+        msg.rotation = current_level_objects.rotation
+        msg.id = current_level_objects.object_id
+        msg.number_of_objects = current_level_objects.number_of_objects
+        
+        self.publisher_.publish(msg)
+        self.publish_level_object_markers()
+        #self.get_logger().info('Publishing %d level objects' % msg.number_of_objects)
+        
+    def publish_level_object_markers(self):
+        for i in range(self.current_level_objects_.number_of_objects):
+            x = self.current_level_objects_.position_x[i]
+            y = self.current_level_objects_.position_y[i]
+            object_id = self.current_level_objects_.object_id[i]
+            
+            self.send_marker(x, y, 2 * i + 1000)
+            self.send_marker(x - 0.15, y, 2 * i + 1000 + 1, 0.15, object_id)
+        
+        
+        
+        # send marker when a new level object is discovered
+    def send_marker(self, x, y, marker_id, scale = 0.1, text = ""):
+        point_in_map_frame = PointStamped()
+        point_in_map_frame.header.frame_id = "/map"
+        point_in_map_frame.header.stamp = self.get_clock().now().to_msg()
+
+        point_in_map_frame.point.x = x
+        point_in_map_frame.point.y = y
+        point_in_map_frame.point.z = 1.0
+        
+        marker = self.create_marker(point_in_map_frame, marker_id, scale, text)
+        self.marker_pub.publish(marker)
+            
+            
+    def create_marker(self, point_stamped, marker_id, scale, text):
+        marker = Marker()
+
+        marker.header = point_stamped.header
+        
+        if text == "":
+            marker.type = marker.SPHERE
+        else:
+            marker.type = marker.TEXT_VIEW_FACING
+            
+        marker.action = marker.ADD
+        marker.id = marker_id
+        marker.lifetime = Duration(sec=3)
+        marker.text = text
+
+        # Set the scale of the marker
+        scale = scale
+        marker.scale.x = scale
+        marker.scale.y = scale
+        marker.scale.z = scale
+
+        # Set the color
+        marker.color.r = 1.0
+        marker.color.g = 0.3
+        marker.color.b = 0.0
+        marker.color.a = 1.0
+
+        # Set the pose of the marker
+        marker.pose.position.x = point_stamped.point.x
+        marker.pose.position.y = point_stamped.point.y
+        marker.pose.position.z = point_stamped.point.z
+
+        return marker
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    level_object_identifier = LevelObjectIdentifier()
+    rclpy.spin(level_object_identifier)
+    
+    level_object_identifier.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
