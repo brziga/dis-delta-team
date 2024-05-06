4dcdbdbc2b35c86ba2719a690378d32ac2723557
Lukas Kneissl <KneisslLukas@web.de>
2024-05-06 19:17:52 +0200

    working on parking node

--- delta_parking/delta_parking/delta_parking.py
+++ delta_parking/delta_parking/delta_parking.py
@@ -341,10 +341,10 @@ class Parking(Node):
         self.approach_final_parking_spot(0.3)
         self.rotate(-self.get_angle_to_detected_ring())
         self.approach_final_parking_spot(0.2)
-        self.rotate(-self.get_angle_to_detected_ring())
-        self.approach_final_parking_spot(0.1)
-        self.rotate(-self.get_angle_to_detected_ring())
-        self.approach_final_parking_spot(0.1)
+        #self.rotate(-self.get_angle_to_detected_ring())
+        #self.approach_final_parking_spot(0.1)
+        #self.rotate(-self.get_angle_to_detected_ring())
+        #self.approach_final_parking_spot(0.1)
         
         
         self.currently_parking = False
