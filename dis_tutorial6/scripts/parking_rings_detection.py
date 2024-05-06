662b58cb31b85670789e3c1c191d6e60bdf47153
Lukas Kneissl <KneisslLukas@web.de>
2024-05-06 20:28:31 +0200

    working on parking node

--- dis_tutorial6/scripts/parking_rings_detection.py
+++ dis_tutorial6/scripts/parking_rings_detection.py
@@ -255,7 +255,7 @@ class RingDetector(Node):
                 timeout = rclpy.duration.Duration(seconds=0.1)
 
                 try:
-                    trans_camera_to_base = self.tf_buffer.lookup_transform("base_link", "top_camera_link", rclpy.time.Time(), timeout)
+                    trans_camera_to_base = self.tf_buffer.lookup_transform("base_link", "top_camera_link", time_now, timeout)
                     point_in_base_frame = tfg.do_transform_point(point_in_arm_camera_frame, trans_camera_to_base)
                 except TransformException as te:
                     self.get_logger().info(f"Cound not get the transform from camera to base: {te}")
@@ -263,7 +263,7 @@ class RingDetector(Node):
                 
 
                 try:
-                    trans = self.tf_buffer.lookup_transform("map", "base_link", rclpy.time.Time(), timeout)
+                    trans = self.tf_buffer.lookup_transform("map", "base_link", time_now, timeout)
                     point_in_map_frame = tfg.do_transform_point(point_in_base_frame, trans)
                     map_frame_x = point_in_map_frame.point.x
                     map_frame_y = point_in_map_frame.point.y
